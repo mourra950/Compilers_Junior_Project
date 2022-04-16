3a5bec683d943fdeda090ef9316f738f03250313
Omar Mohamed <mourra24@gmail.com>
2022-04-16 11:04:55 +0200

    test
    test

--- lex.py
+++ lex.py
@@ -1,3 +1,7 @@
+import sys
+import enum
+
+
 class Lexer:
     def __init__(self, input):
         # Source code to lex as a string. Append a newline to simplify lexing/parsing the last token/statement.
@@ -22,16 +26,112 @@ class Lexer:
 
     # Invalid token found, print error message and exit.
     def abort(self, message):
-        pass
+        sys.exit("Lexing error. " + message)
 
     # Skip whitespace except newlines, which we will use to indicate the end of a statement.
     def skipWhitespace(self):
-        pass
+        while self.curChar == ' ' or self.curChar == '\t' or self.curChar == '\r':
+            self.nextChar()
 
     # Skip comments in the code.
     def skipComment(self):
-        pass
+        if self.curChar == '#':
+            while self.curChar != '\n':
+                self.nextChar()
 
     # Return the next token.
     def getToken(self):
-        pass
+        self.skipWhitespace()
+        self.skipComment()
+        token = None
+
+        # Check the first character of this token to see if we can decide what it is.
+        # If it is a multiple character operator (e.g., !=), number, identifier, or keyword then we will process the rest.
+        if self.curChar == '+':
+            token = Token(self.curChar, TokenType.PLUS)
+        elif self.curChar == '-':
+            token = Token(self.curChar, TokenType.MINUS)
+        elif self.curChar == '*':
+            token = Token(self.curChar, TokenType.ASTERISK)
+        elif self.curChar == '/':
+            token = Token(self.curChar, TokenType.DIVIDE)
+        elif self.curChar.isdigit():
+            # Leading character is a digit, so this must be a number.
+            # Get all consecutive digits and decimal if there is one.
+            startPos = self.curPos
+            while self.peek().isdigit():
+                self.nextChar()
+            if self.peek() == '.':  # Decimal!
+                self.nextChar()
+
+                # Must have at least one digit after decimal.
+                if not self.peek().isdigit():
+                    # Error!
+                    self.abort("Illegal character in number.")
+                while self.peek().isdigit():
+                    self.nextChar()
+
+            # Get the substring.
+            tokText = self.source[startPos: self.curPos + 1]
+            token = Token(tokText, TokenType.NUM)
+        elif self.curChar.isalpha():
+            # Leading character is a letter, so this must be an identifier or a keyword.
+            # Get all consecutive alpha numeric characters.
+            startPos = self.curPos
+            while self.peek().isalnum():
+                self.nextChar()
+
+            # Check if the token is in the list of keywords.
+            # Get the substring.
+            tokText = self.source[startPos: self.curPos + 1]
+            keyword = Token.checkIfKeyword(tokText)
+            if keyword == None:  # Identifier
+                token = Token(tokText, TokenType.ID)
+            else:   # Keyword
+                token = Token(tokText, keyword)
+        elif self.curChar == '(':
+            token = Token(self.curChar, TokenType.OPENBRACKET)
+        elif self.curChar == ')':
+            token = Token(self.curChar, TokenType.CLOSEDBRACKET)
+        elif self.curChar == '\n':
+            token = Token(self.curChar, TokenType.NEWLINE)
+        elif self.curChar == '\0':
+            token = Token('', TokenType.EOF)
+        else:
+            # Unknown token!
+            self.abort("Unknown token: " + self.curChar)
+
+        self.nextChar()
+        return token
+
+
+# Token contains the original text and the type of token.
+class Token:
+    def __init__(self, tokenText, tokenKind):
+        # The token's actual text. Used for identifiers, strings, and numbers.
+        self.text = tokenText
+        # The TokenType that this token is classified as.
+        self.kind = tokenKind
+
+    @staticmethod
+    def checkIfKeyword(tokenText):
+        for kind in TokenType:
+            # Relies on all keyword enum values being 1XX.
+            if kind.name == tokenText and kind.value >= 100 and kind.value < 200:
+                return kind
+        return None
+
+
+# TokenType is our enum for all the types of tokens.
+class TokenType(enum.Enum):
+    EOF = 0
+    NUM = 1
+    ID = 2
+    EQ = 3
+    PLUS = 4
+    MINUS = 5
+    ASTERISK = 6
+    DIVIDE = 7
+    NEWLINE = 8
+    OPENBRACKET = 9
+    CLOSEDBRACKET = 10
--- teenytiny.py
+++ teenytiny.py
@@ -1,58 +1,14 @@
 from lex import *
-import enum
-
-# Token contains the original text and the type of token.
-
-
-class Token:
-    def __init__(self, tokenText, tokenKind):
-        # The token's actual text. Used for identifiers, strings, and numbers.
-        self.text = tokenText
-        # The TokenType that this token is classified as.
-        self.kind = tokenKind
-
-# TokenType is our enum for all the types of tokens.
-
-
-class TokenType(enum.Enum):
-    EOF = -1
-    NEWLINE = 0
-    NUMBER = 1
-    IDENT = 2
-    STRING = 3
-    # Keywords.
-    LABEL = 101
-    GOTO = 102
-    PRINT = 103
-    INPUT = 104
-    LET = 105
-    IF = 106
-    THEN = 107
-    ENDIF = 108
-    WHILE = 109
-    REPEAT = 110
-    ENDWHILE = 111
-    # Operators.
-    EQ = 201
-    PLUS = 202
-    MINUS = 203
-    ASTERISK = 204
-    SLASH = 205
-    EQEQ = 206
-    NOTEQ = 207
-    LT = 208
-    LTEQ = 209
-    GT = 210
-    GTEQ = 211
 
 
 def main():
-    input = "LET foobar = 123"
+    input = "int"
     lexer = Lexer(input)
 
-    while lexer.peek() != '\0':
-        print(lexer.curChar)
-        lexer.nextChar()
+    token = lexer.getToken()
+    while token.kind != TokenType.EOF:
+        print(token.kind)
+        token = lexer.getToken()
 
 
 main()
