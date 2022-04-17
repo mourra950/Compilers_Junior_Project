1bbdf28c5666a09aabd2b233239d6007c7379234
Omar Mohamed <mourra24@gmail.com>
2022-04-17 02:29:10 +0200

    updated the dfa
    reduced the inputs to one letters only

--- dfa.py
+++ dfa.py
@@ -1,20 +1,20 @@
 from automata.fa.dfa import DFA
-# DFA which matches all binary strings ending in an odd number of '1's
+# DFA which matches all binary strings ending in an odd Nber of '1's
 dfa = DFA(
     states={'start', '1', '2', '3', '4', '5', '6', 'dead'},
-    input_symbols={'+', '-', '/', '*', 'ID', 'NUM'},
+    input_symbols={'+', '-', '/', '*', 'I', 'N'},
     transitions={
-        'start': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
-        '1': {'+': '3', '-': '4', '*': '5', '/': '6', 'ID': 'dead', 'NUM': 'dead'},
-        '2': {'+': '3', '-': '4', '*': '5', '/': '6', 'ID': 'dead', 'NUM': 'dead'},
-        '3': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
-        '4': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
-        '5': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
-        '6': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
-        'Dead': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': 'dead', 'NUM': 'dead'}
+        'start': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'I': '1', 'N': '2'},
+        '1': {'+': '3', '-': '4', '*': '5', '/': '6', 'I': 'dead', 'N': 'dead'},
+        '2': {'+': '3', '-': '4', '*': '5', '/': '6', 'I': 'dead', 'N': 'dead'},
+        '3': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'I': '1', 'N': '2'},
+        '4': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'I': '1', 'N': '2'},
+        '5': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'I': '1', 'N': '2'},
+        '6': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'I': '1', 'N': '2'},
+        'dead': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'I': 'dead', 'N': 'dead'}
     },
-    initial_state='q0',
-    final_states={'q1'}
+    initial_state='start',
+    final_states={'1', '2'}
 )
 
-print(dfa.read_input('0'))
+print(dfa.read_input('I+I+N/I-N'))
