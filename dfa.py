6b1675195f750dce9b94e88bcc50751e3ed661b0
Omar Mohamed <mourra24@gmail.com>
2022-04-17 23:21:38 +0200

    created function dfa
    function dfa get a list of tokens from teeny language file and starts proccessing the dfa

--- dfa.py
+++ dfa.py
@@ -1,26 +1,37 @@
 from automata.fa.dfa import DFA
 # DFA which matches all binary strings ending in an odd Nber of '1's
-dfa = DFA(
-    states={'start', '1', '2', '3', '4', '5', '6', 'dead'},
-    input_symbols={'+', '-', '/', '*', 'ID', 'NUM'},
-    transitions={
-        'start': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
-        '1': {'+': '3', '-': '4', '*': '5', '/': '6', 'ID': 'dead', 'NUM': 'dead'},
-        '2': {'+': '3', '-': '4', '*': '5', '/': '6', 'ID': 'dead', 'NUM': 'dead'},
-        '3': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
-        '4': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
-        '5': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
-        '6': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
-        'dead': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': 'dead', 'NUM': 'dead'}
-    },
-    initial_state='start',
-    final_states={'1', '2'}
-)
-a = ['ID', '+', 'NUM']
-print("final state is :"+str(dfa.read_input(a)))
-b = dfa.read_input_stepwise(a)
-count = 0
-for i in b:
-    print("step: "+str(count)+" is state "+str(i))
-    count += 1
-# lalalalala
+
+
+def dfafunction(a):
+    dfa = DFA(
+        states={'start', '1', '2', '3', '4', '5', '6', 'dead'},
+        input_symbols={'+', '-', '/', '*', 'ID', 'NUM'},
+        transitions={
+            'start': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
+            '1': {'+': '3', '-': '4', '*': '5', '/': '6', 'ID': 'dead', 'NUM': 'dead'},
+            '2': {'+': '3', '-': '4', '*': '5', '/': '6', 'ID': 'dead', 'NUM': 'dead'},
+            '3': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
+            '4': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
+            '5': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
+            '6': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': '1', 'NUM': '2'},
+            'dead': {'+': 'dead', '-': 'dead', '/': 'dead', '*': 'dead', 'ID': 'dead', 'NUM': 'dead'}
+        },
+        initial_state='start',
+        final_states={'1', '2'}
+    )
+    b = []
+    if dfa.accepts_input(a):
+        print("final state is :"+str(dfa.read_input(a)))
+    else:
+        print("not valid")
+    try:
+        b = dfa.read_input_stepwise(a)
+    except:
+        print("not valid")
+    count = 0
+    try:
+        for i in b:
+            print("step: "+str(count)+" is state "+str(i))
+            count += 1
+    except:
+        print("the last state is not valid")
