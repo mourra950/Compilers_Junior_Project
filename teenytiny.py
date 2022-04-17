31f205d93a10466e5b320150b552e39ba0ad5579
Omar Mohamed <mourra24@gmail.com>
2022-04-17 23:20:36 +0200

    updated the teenytiny.py
    added conditions to the while loop to append tokens into the array to use it in the dfa.py

--- teenytiny.py
+++ teenytiny.py
@@ -1,15 +1,28 @@
+import dfa
 from lex import *
 
 
 def main():
-    input = "++"
-    lexer = Lexer(input)
+    a = []
+    input1 = input("enter the lexical expression to analyze : ")
+    lexer = Lexer(input1)
 
     token = lexer.getToken()
     while token.kind != TokenType.EOF:
-        print(token.kind)
-
+        if(token.kind == TokenType.NUM):
+            a.append('NUM')
+        if(token.kind == TokenType.ID):
+            a.append('ID')
+        if(token.kind == TokenType.PLUS):
+            a.append('+')
+        if(token.kind == TokenType.ASTERISK):
+            a.append('*')
+        if(token.kind == TokenType.MINUS):
+            a.append('-')
+        if(token.kind == TokenType.DIVIDE):
+            a.append('/')
         token = lexer.getToken()
+    dfa.dfafunction(a)
 
 
 main()
