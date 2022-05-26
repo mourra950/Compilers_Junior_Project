ea14fb35ab090aa4195de7198b58511c5da0d6de
Omar Mohamed <mourra24@gmail.com>
2022-05-26 11:21:24 +0200

    completed parsing tree drawing
    implemented small

--- nltk2.py
+++ nltk2.py
@@ -1,8 +1,11 @@
 import nltk as nltk
-import nltk.draw as draw 
-def main():
-    txt='( Expression ( Term ( factor number )  ( termdash ε )  ( Expressiondash ( addop + )  ( Term ( factornumber)  ( termdash ( mulop * )  ( factor number )  ( termdash ε )  ( Expressiondash ( addop - )  ( Term ( factor number )  ( termdash ε )  ( Expressiondash ε) )))))))'
-    tree=nltk.Tree.fromstring(txt)
+import nltk.draw as draw
+
+
+def drawparsingtree(txt):
+    # txt = '( ( Expression ( ( Term ( factor number ) ( termdash ( mulop * ) ( factor number ) ( termdash ε ) ) ) ( Expressiondash ( addop - ) ( Term ( factor number ) ( termdash ε ) ) ( Expressiondash ε ) ))))'
+    tree = nltk.Tree.fromstring(txt)
     draw.draw_trees(tree)
-    
-main()
+
+
+
